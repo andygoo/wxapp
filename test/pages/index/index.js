040fd2715a59b06ebbca7ed0ca90b5546ed29f9f
[xiaowenjie] <[xiaowenjie@haoche51.com]>
2016-09-29 16:13:52 +0800

    init

---
+++ test/pages/index/index.js
@@ -0,0 +1,99 @@
+
+var pageData = {}
+pageData.data = {}
+
+
+//silder
+Object.assign(pageData.data, {
+  imgUrls: [
+    'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+    'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+    'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+  ],
+  indicatorDots: true,
+  autoplay: false,
+  interval: 5000,
+  duration: 1000
+});
+
+
+//actionSheet
+Object.assign(pageData.data, {
+  actionSheetHidden: true,
+  actionSheetItems: ['item1', 'item2', 'item3', 'item4']
+});
+pageData.actionSheetTap = function(e) {
+  this.setData({
+    actionSheetHidden: !this.data.actionSheetHidden
+  })
+}
+pageData.actionSheetChange = function(e) {
+  this.setData({
+    actionSheetHidden: !this.data.actionSheetHidden
+  })
+}
+pageData.bindItemTap = function(e){
+  console.log('tap ' + e.currentTarget.dataset.name)
+}
+
+//modal
+Object.assign(pageData.data, {
+  modalHidden: true,
+  modalHidden2: true,
+  hidden: true
+});
+pageData.modalTap = function(e) {
+  this.setData({
+    modalHidden: false
+  })
+}
+pageData.modalChange = function(e) {
+  this.setData({
+    modalHidden: true
+  })
+}
+pageData.modalTap2 = function(e) {
+  this.setData({
+    modalHidden2: false
+  })
+}
+pageData.modalChange2 = function(e) {
+  this.setData({
+    modalHidden2: true
+  })
+}
+
+//toast
+Object.assign(pageData.data, {
+  toastHidden: true
+});
+pageData.toastChange = function(e) {
+  this.setData({toastHidden: true})
+}
+pageData.toastTap = function(e) {
+  this.setData({toastHidden: false})
+}
+
+//loading
+Object.assign(pageData.data, {
+  loadingHidden: true
+});
+pageData.loadingChange = function () {
+  this.setData({
+    loadingHidden: true
+  })
+}
+pageData.loadingTap = function () {
+  this.setData({
+    loadingHidden: false
+  })
+
+  var that = this
+  setTimeout(function () {
+    that.setData({
+      loadingHidden: true
+    })
+  }, 1500)
+}
+
+Page(pageData)
---
+++ test/pages/index/navigate.js
@@ -0,0 +1,15 @@
+
+
+Page({
+  onLoad: function(options) {
+    this.setData({
+      title: options.title
+    })
+  },
+  formSubmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset: function() {
+    console.log('form发生了reset事件')
+  }
+})
